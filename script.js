58b3d5c01b56735bd3cc0dfdd33273e7f59a3b37
alearanc <alexisarancibia97@gmail.com>
2024-05-08 16:45:27 -0300

    Modify logic in js file to make the application fully functional

--- script.js
+++ script.js
@@ -1,49 +1,79 @@
 function addTask() {
-    let $input = document.getElementById("task-input")
-    let task = $input.value
-    if (task === "") {
-        alert("The task is empty!")
+    let $input = document.getElementById('task-input')
+    let taskText = $input.value
+    if (taskText === '') {
+        alert('The task is empty!')
         return
     }
-    let $pendingTasks = document.getElementById("pending-tasks")
-    let $li = document.createElement("li")
-    $li.appendChild(document.createTextNode(task))
-    let $doneButton = document.createElement("button")
-    $doneButton.textContent = "Done"
-    $doneButton.addEventListener("click", () => {
+    createTask(taskText)
+    $input.value = ''
+}
+
+function createTask(taskText) {
+    let $pendingTasks = document.getElementById('pending-tasks')
+    let $li = document.createElement('li')
+
+    $li.appendChild(document.createTextNode(taskText))
+
+    createDoneOption($li)
+    createDeleteOption($li)
+    
+    $pendingTasks.appendChild($li)
+}
+
+function createDoneOption($li) {
+    let $doneButton = document.createElement('button')
+    $doneButton.textContent = 'Done'
+    $doneButton.addEventListener('click', () => {
         doneTask($li)
     })
     $li.appendChild($doneButton)
-    let $deleteButton = document.createElement("button")
-    $deleteButton.textContent = "Delete"
-    $deleteButton.addEventListener("click", () => {
+}
+
+function createDeleteOption($li) {
+    let $deleteButton = document.createElement('button')
+    $deleteButton.textContent = 'Delete'
+    $deleteButton.addEventListener('click', () => {
         removeTask($li)
     })
     $li.appendChild($deleteButton)
-    $pendingTasks.appendChild($li)
-    $input.value = ""
 }
 
-function doneTask(taskElement) {
-    let $doneTasks = document.getElementById("done-tasks")
-    let $undoneButton = document.createElement("button")
-    $undoneButton.textContent = "Undone"
-    $undoneButton.addEventListener("click", () => {
+function createUndoOption($li) {
+    let $undoneButton = document.createElement('button')
+    $undoneButton.textContent = 'Undo'
+    $undoneButton.addEventListener('click', () => {
         undoneTask($li)
     })
-    let $doneButton = taskElement.querySelector("button")
-    if ($doneButton && $doneButton.textContent === "Done")
-        taskElement.removeChild($doneButton)
-    $doneTasks.appendChild(taskElement)
+    $li.appendChild($undoneButton)
+}
+
+function doneTask($li) {
+    let $doneTasks = document.getElementById('done-tasks')
+    
+    let $buttons = $li.querySelectorAll("button")
+    $buttons.forEach(button => button.remove())
+    
+    createUndoOption($li)
+    createDeleteOption($li)
+
+    $doneTasks.appendChild($li)
 }
 
 function removeTask(taskElement) {
     taskElement.remove()
 }
 
-function undoneTask(taskElement) {
+function undoneTask($li) {
+    let $buttons = $li.querySelectorAll("button")
+    $buttons.forEach(button => button.remove())
 
+    createDoneOption($li)
+    createDeleteOption($li)
+
+    let $pendingTasks = document.getElementById("pending-tasks")
+    $pendingTasks.appendChild($li)
 }
 
-const $submitButton = document.getElementById("submit-button")
-$submitButton.addEventListener("click", addTask)
+const $submitButton = document.getElementById('submit-button')
+$submitButton.addEventListener('click', addTask)
